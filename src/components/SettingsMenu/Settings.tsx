6bec9f5a9c77ed6fdca06a988868924406f46b16
Wes Marc <weslei047@gmail.com>
2022-11-16 01:35:19 -0300

    Finished static header and settings menu

--- src/components/SettingsMenu/Settings.tsx
+++ src/components/SettingsMenu/Settings.tsx
@@ -1,18 +1,30 @@
 import { GearSix } from 'phosphor-react';
+import { useState } from 'react';
 
 import styles from './Settings.module.css';
 import { SalaryModal } from './SettingsModal/SalaryModal';
 
 export function Settings() {
+    const [ display, setDisplay ] = useState('none');
+
+    function handleToggleModal() {
+        if (display === 'none') {
+            setDisplay('block');
+        } else {
+            setDisplay('none');
+        }
+    }
+
     return (
         <>
             <div className={styles.setting}>
                 <GearSix
                     className={styles.icon}
                     color='#c4c4cc'
-                    size={24} 
+                    size={24}
+                    onClick={handleToggleModal}
                 />
-                <SalaryModal />
+                <SalaryModal display={display} />
             </div>
         </>
     )
--- src/components/SettingsMenu/SettingsModal/SalaryModal.tsx
+++ src/components/SettingsMenu/SettingsModal/SalaryModal.tsx
@@ -1,29 +1,35 @@
 import { useState } from 'react';
 import styles from './SalaryModal.module.css';
 
-export function SalaryModal() {
+interface settingsModal {
+    display: string;
+}
+
+export function SalaryModal({ display }: settingsModal) {
     const [amount, setAmount] = useState(0);
 
     return (
-        <form className={styles.modal}>
-            <div className={styles.input_wrapper}>
-                <label htmlFor="salaryInput">Salário</label>
-                <span id={styles.currency}>
-                    <input
-                        type="number"
-                        name="salaryInput"
-                        id={styles.salaryInput}
-                        min="0.00"
-                        max="10000.00"
-                        step="0.01"
-                        value={amount.toFixed(2)}
-                        onChange={event => setAmount(Number(event.target.value))}
-                    />
-                </span>
-            </div>
-            <div className={styles.cta_wrapper}>
-                <button className="cta-salvar" type="submit">Salvar</button>
-            </div>
-        </form>
+        <div style={{display: display}}>
+            <form className={styles.modal}>
+                <div className={styles.input_wrapper}>
+                    <label htmlFor="salaryInput">Salário</label>
+                    <span id={styles.currency}>
+                        <input
+                            type="number"
+                            name="salaryInput"
+                            id={styles.salaryInput}
+                            min="0.00"
+                            max="10000.00"
+                            step="0.01"
+                            value={amount.toFixed(2)}
+                            onChange={event => setAmount(Number(event.target.value))}
+                        />
+                    </span>
+                </div>
+                <div className={styles.cta_wrapper}>
+                    <button className="cta-salvar" type="submit">Salvar</button>
+                </div>
+            </form>
+        </div>
     );
 };
